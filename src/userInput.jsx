dda67bbd92620a0d92e2bae20faa49ca8f44f65a
Ivana <ivana.puskaric2@gmail.com>
2024-05-12 21:58:22 +0200

    creating file userInput.jsx and exporting it

---
+++ src/userInput.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function userInput({onComplete}){
+    return <form onSubmit={(event)=>{
+      event.preventDefault(); 
+      const formElement = event.target;
+      const inputElement =  formElement.search;
+      const inputValue = inputElement.value;
+      if(!inputValue) return
+      onComplete(inputValue);
+      console.log({formElement, inputElement});
+    }}>
+      <input id="search" type="text" placeholder="e.g. reduxjs"/>
+      <button type="submit" >Go!</button>
+    </form>
+  }
+
+  export default userInput;
